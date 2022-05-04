2138e666b5b0f2f1a6ead10539259127cc9fbc32
Dream0804 <dream18210@gmail.com>
2022-05-04 22:12:24 +0900

    Validation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
+// import 'https://fb.me/react-with-addons-0.14.7.js';
+
 function useTouchedFields() {
   const [touchedFields, setTouchedFields] = useState({ all: false })
   
@@ -25,75 +27,76 @@ function useTouchedFields() {
 
 const ContactForm = () => {
   const [bindField, setAllFieldsTouched] = useTouchedFields()
-  // const [touchedFields, setTouchedFields] = useState({ all: false })
   
-  // const setFieldTouched = (event) => {
-  //   setTouchedFields((touchedFields) => (
-  //     {...touchedFields, [event.target.name]: true}
-  //   ))
-  // }
-  
+  const[add, remove] = useState();
+
   return (
-    <div className="container">
+    <div className='container'>
       <form
         onSubmit = {(event) => {
           event.preventDefault()
           const formData = new FormData(event.target)
           const data = Object.fromEntries(formData.entries())
-          console.log(data) 
+          console.log(data)
         }}
       >
-        <fieldset>
-          <label htmlFor="name">Name *</label>
+        <fieldset className="text-input">
           <input 
             id="name" 
             name="name" 
-            type="text"           
-            // onBlur={setFieldTouched}
-            // data-touched={touchedFields.name}
+            type="text"
+            onFocus={() => remove(true)}
+            onBlur={() => remove(false)}
+            // data-touched={touchedFields.message}
             {...bindField("name")}
             required
             />
+            <label htmlFor="name" className={add ? "label-focus" : ""}>Name *</label>
           <div className="form-required-message">A name is required.</div>
         </fieldset>
-        <fieldset>
-          <label htmlFor="email">Email *</label>
+        <fieldset className="text-input">
           <input 
             id="email" 
             name="email" 
             type="email" 
-            // onBlur={setFieldTouched}
-            // data-touched={touchedFields.email}
             {...bindField("email")}
             required  
           />
+          <label htmlFor="email">Email *</label>
           <div className="form-required-message">A valid email is required.</div>
         </fieldset>
-        <fieldset>
-          <label htmlFor="tel">Phone *</label>
+        <fieldset className="text-input">
+          <input 
+            id="password" 
+            name="password" 
+            type="password" 
+            {...bindField("password")}
+            required  
+          />
+          <label htmlFor="password">Password *</label>
+          <div className="form-required-message">A valid password is required.</div>
+        </fieldset>
+        <fieldset className="text-input">
           <input 
             id="tel" 
             name="tel" 
-            type="tel" 
-            // onBlur={setFieldTouched}
-            // data-touched={touchedFields.tel}                    
+            type="tel"               
             {...bindField("tel")}
             required 
           />
+          <label htmlFor="tel">Phone *</label>
           <div className="form-required-message">A valid phone number is required.</div>
         </fieldset>
-        <fieldset>
-          <label htmlFor="message">Message *</label>
+        <fieldset className="text-input">
           <textarea 
             id="message" 
             name="message" 
             type="textarea" 
-            rows="3" 
-            // onBlur={setFieldTouched}
-            // data-touched={touchedFields.message}
+            rows="3"
             {...bindField("message")}
             required 
           />
+          <label htmlFor="message">Message *</label>
           <div className="form-required-message">A message is required.</div>
         </fieldset>
         <button onClick={() => setAllFieldsTouched()} type="submit">
